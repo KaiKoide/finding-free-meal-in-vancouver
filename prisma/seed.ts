c74fc555fa117f71d96dda2eba5979c2e2425889
Kai <kkai.chien0813@gmail.com>
2024-06-30 15:35:58 -0700

    chore: insert a record into a table

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function seed() {
+	const favorite = await prisma.favoriteList.create({
+		data: {
+			name: 'test',
+			lat: '49.28967576079232',
+			lon: '-123.13857731195915',
+		},
+	});
+
+	console.log(favorite);
+}
+
+seed()
+	.then(() => {
+		prisma.$disconnect();
+		process.exit(0);
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
