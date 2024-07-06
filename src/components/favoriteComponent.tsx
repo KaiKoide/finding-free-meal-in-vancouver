de29c69dd00d528aab4308672c8f8dcccd15383e
Kai <kkai.chien0813@gmail.com>
2024-07-06 00:19:48 -0700

    feat: add buttons on favorite list

--- src/components/favoriteComponent.tsx
+++ src/components/favoriteComponent.tsx
@@ -1,14 +1,17 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
-import { fetchFavoriteList } from '@/lib/database/api';
-import type FavoriteListData from '@/types/favoriteListData';
+import { removeFavorite } from '@/lib/database/api';
 import useFavoriteStore from '@/store/useFavoriteStore';
+import { Button } from './ui/button';
 
 const FavoriteContent = () => {
 	const favoriteList = useFavoriteStore((state) => state.favoriteList);
 	const fetchFavorites = useFavoriteStore((state) => state.fetchFavorite);
+	const removeFavoriteFromStore = useFavoriteStore(
+		(state) => state.removeFavoriteFromStore,
+	);
 
 	useEffect(() => {
 		fetchFavorites();
@@ -16,10 +19,33 @@ const FavoriteContent = () => {
 
 	console.log('favoriteList', favoriteList);
 
+	function handleRemoveClick(favoriteId: number) {
+		try {
+			if (favoriteList) {
+				removeFavorite(favoriteId);
+				removeFavoriteFromStore(favoriteId);
+			}
+		} catch (error) {
+			console.error('Something wrong!');
+		}
+	}
+
 	return (
 		<div>
 			{favoriteList.map((favorite) => (
-				<p key={favorite.id}>{favorite.name}</p>
+				<div key={favorite.id} className='flex flex-col gap-2 mt-5'>
+					<p>{favorite.name}</p>
+					<div className='flex gap-3 justify-end'>
+						<Button className='capitalize'>get direction</Button>
+						<Button
+							variant='destructive'
+							className='capitalize'
+							onClick={() => handleRemoveClick(favorite.id)}
+						>
+							remove
+						</Button>
+					</div>
+				</div>
 			))}
 		</div>
 	);
