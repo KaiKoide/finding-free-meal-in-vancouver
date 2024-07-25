099ad3086481eca63929cf9046f26634a708034f
Kai <kkai.chien0813@gmail.com>
2024-07-24 22:10:12 -0700

    feat: style top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,15 @@ export default function Home() {
 					alt='map'
 					width={1920}
 					height={1080}
-					className='min-h-screen object-cover'
+					className='min-h-screen object-cover brightness-50'
 				/>
-				<div className='absolute inset-0 flex flex-col items-center justify-center text-white'>
-					<h1 className='text-4xl font-bold mb-4'>
+				<div className='absolute inset-0 flex flex-col items-center justify-center text-white text-center'>
+					<h1 className='text-4xl md:text-5xl lg:text-6xl font-bold mb-4'>
 						Finding Free Meal in Vancouver
 					</h1>
-					<p className='text-2xl'>Discover Nearby Free or Low-Cost Meals!</p>
+					<h2 className='text-2xl md:text-3xl lg:text-4xl'>
+						Discover Nearby Free or Low-Cost Meals!
+					</h2>
 				</div>
 			</div>
 		</div>
