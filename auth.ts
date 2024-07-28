74e1912a72b9180480439e46df9ec47780248ae6
Kai <kkai.chien0813@gmail.com>
2024-07-27 22:20:03 -0700

    feat: add user id to session

--- auth.ts
+++ auth.ts
@@ -3,4 +3,10 @@ import GitHub from 'next-auth/providers/github';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
 	providers: [GitHub],
+	callbacks: {
+		session: ({ session, token }) => {
+			session.user.id = token.sub as string;
+			return session;
+		},
+	},
 });
