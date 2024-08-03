45f19135cd3021c9032671f80e92a189edfe01e1
Kai <kkai.chien0813@gmail.com>
2024-08-03 11:46:26 -0700

    feat: implement loading animation on map

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -3,7 +3,7 @@ import { LoaderCircle } from 'lucide-react';
 export const Loading = () => {
 	return (
 		<div className='flex justify-center items-center h-full'>
-			<LoaderCircle className='animate-spin' />
+			<LoaderCircle className='animate-spin w-10 h-10 text-white' />
 		</div>
 	);
 };
